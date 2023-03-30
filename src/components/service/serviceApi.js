d966c9ba203ad53370eca564c8d2d28f4002b293
Aratskov <ifromukraine2022@gmail.com>
2023-03-30 14:12:06 +0300

    init

--- src/components/service/serviceApi.js
+++ src/components/service/serviceApi.js
@@ -1,18 +1,17 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://pixabay.com';
+axios.defaults.baseURL = 'https://pixabay.com/api';
 const API_KEY = '34039556-33388eb553809646fe0c9f5bf';
 
 export const getSearchImages = (q, page) => {
   return axios
-    .get('/api', {
+    .get('/', {
       params: {
         q,
         page,
         key: API_KEY,
         image_type: 'photo',
         orientation: 'horizontal',
-        safesearch: true,
         per_page: 12,
       },
     })
